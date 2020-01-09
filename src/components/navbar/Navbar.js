6641052c480e847d4314676445f1d736de247097
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-01-09 16:53:58 +0000

    Add react router and navbar and error page

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <header className='header'>
+        <h1>Dogs Breeds</h1>
+        <Link className='link' to='/'>
+          Home
+        </Link>{" "}
+        ||{" "}
+        <Link className='link' to='/about'>
+          About
+        </Link>
+      </header>
+    );
+  }
+}
+
+export default Navbar;
